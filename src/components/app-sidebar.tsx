c7b36a5fb30129a5f9f903c8c717c4ab417e21c1
Abhi Kumar <abhi_yallapu@tecnics.com>
2025-02-04 18:56:47 +0530

    Refactor AppSidebar component to use Sidebar component from ui/sidebar module

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,36 +1,288 @@
-import { Moon, Sun } from "lucide-react";
-import { Button } from "./button";
+import { useState, useEffect } from "react";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "./dropdown-menu";
-import { useTheme } from "./theme-provider";
+  Handshake,
+  Swords,
+  Home,
+  LogOut,
+  CircleUserRound,
+  Search,
+  Settings,
+  User as Profile,
+  ChevronDown,
+  HelpCircle,
+  Info,
+  Users,
+  Send,
+} from "lucide-react";
 
-export function ModeToggle() {
-  const { theme, setTheme } = useTheme();
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
+  SidebarHeader,
+} from "./ui/sidebar";
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from "@radix-ui/react-collapsible";
+import { Tooltip, TooltipTrigger, TooltipContent } from "./ui/tooltip";
+import { useTheme } from "./ui/theme-provider";
+import { useSidebar } from "./ui/sidebar";
+import { useLocation } from "react-router-dom";
+
+const items = [
+  {
+    title: "Home",
+    url: "/home",
+    icon: Home,
+  },
+  {
+    title: "My Profile",
+    url: "#",
+    icon: Profile,
+  },
+  {
+    title: "Matches",
+    url: "/matches",
+    icon: Swords,
+  },
+  {
+    title: "Teams",
+    url: "#",
+    icon: Handshake,
+  },
+  {
+    title: "Groups",
+    url: "#",
+    icon: Users,
+  },
+  {
+    title:"Friends",
+    url: "#",
+    icon: Users,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+const helpItems = [
+  {
+    title: "Support",
+    url: "/support",
+    icon: HelpCircle,
+  },
+  {
+    title: "Feedback",
+    url: "/feedback",
+    icon: Send,
+  },
+];
+
+const footerItems = [
+  {
+    title: "Account",
+    url: "/account",
+    icon: CircleUserRound,
+  },
+  {
+    title: "Sign Out",
+    url: "/signout",
+    icon: LogOut,
+  },
+];
+
+export function AppSidebar() {
+  const [activeItem, setActiveItem] = useState<string | null>(null);
+  const { theme } = useTheme();
+  const { state } = useSidebar();
+  const location = useLocation();
+
+  useEffect(() => {
+    const currentPath = location.pathname;
+    const activeMenuItem = items.find((item) => item.url === currentPath);
+    if (activeMenuItem) {
+      setActiveItem(activeMenuItem.title);
+    }
+  }, [location]);
+
+  const handleItemClick = (title: string) => {
+    setActiveItem(title);
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className={`${theme === "light" ? " border-black text-white" : ""}`}>
-          <Sun className={`h-[1.2rem] w-[1.2rem] transition-all ${theme === "dark" ? "rotate-0 scale-100 text-white" : "-rotate-90 scale-0 text-gray-800"}`} />
-          <Moon className={`absolute h-[1.2rem] w-[1.2rem] transition-all ${theme === "dark" ? "rotate-90 scale-0 text-gray-800" : "rotate-0 scale-100 text-black"}`} />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className={`${theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-black"}`}>
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Sidebar
+      side="left"
+      variant="sidebar"
+      collapsible="icon"
+      className={`w-[var(--sidebar-width)] ${
+        state === "collapsed" ? "transition-width duration-300" : ""
+      } ${theme === "dark" ? "bg-gray-900 text-gray-300" : "bg-gray-100 text-gray-800"} overflow-hidden border-r-2 ${theme === "dark" ? "border-gray-800" : "border-gray-400"}`}
+    >
+      <SidebarHeader className="p-4"></SidebarHeader>
+      <SidebarContent className="overflow-hidden hover:overflow-y-auto">
+        <SidebarGroup>
+          <SidebarGroupLabel
+            className={`left-0 text-lg font-bold p-8 mt-4 duration-700 ${
+              theme === "dark" ? "text-gray-400 hover:text-gray-200" : "text-gray-600 hover:text-gray-800"
+            }`}
+          >
+          Fantasy League
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem
+                  key={item.title}
+                  className={`flex items-center p-2 transition-colors duration-200 ${
+                    activeItem === item.title
+                      ? theme === "dark"
+                        ? "bg-gray-700"
+                        : "bg-gray-300"
+                      : theme === "dark"
+                      ? "hover:bg-gray-800"
+                      : "hover:bg-gray-200"
+                  }`}
+                >
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <SidebarMenuButton asChild className="flex items-center w-full text-inherit no-underline text-lg">
+                        <a
+                          href={item.url}
+                          onClick={() => handleItemClick(item.title)}
+                          className="flex items-center w-full"
+                        >
+                          <item.icon className="mr-2 text-2xl" />
+                          <span className="ml-2">{item.title}</span>
+                        </a>
+                      </SidebarMenuButton>
+                    </TooltipTrigger>
+                    <TooltipContent
+                      side="right"
+                      align="center"
+                      hidden={state !== "collapsed"}
+                      className={`${
+                        theme === "dark" ? "bg-white text-black" : "bg-black text-white"
+                      }`}
+                    >
+                      {item.title}
+                    </TooltipContent>
+                  </Tooltip>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        {state !== "collapsed" && (
+          <Collapsible defaultOpen className="group text-center justify-center items-center">
+            <SidebarGroup>
+              <SidebarGroupLabel asChild className="flex items-center cursor-pointer text-lg justify-center">
+                <CollapsibleTrigger className="flex items-center w-full justify-center">
+                  Help
+                  <ChevronDown className="ml-auto transition-transform duration-200 group-data-[state=open]:rotate-180" />
+                </CollapsibleTrigger>
+              </SidebarGroupLabel>
+              <CollapsibleContent>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    {helpItems.map((item) => (
+                      <SidebarMenuItem
+                        key={item.title}
+                        className={`flex items-center p-2 transition-colors duration-200 ${
+                          activeItem === item.title
+                            ? theme === "dark"
+                              ? "bg-gray-700"
+                              : "bg-gray-300"
+                            : theme === "dark"
+                            ? "hover:bg-gray-800"
+                            : "hover:bg-gray-200"
+                        }`}
+                      >
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <SidebarMenuButton asChild className="flex items-center w-full text-inherit no-underline text-lg">
+                              <a
+                                href={item.url}
+                                onClick={() => handleItemClick(item.title)}
+                                className="flex items-center w-full"
+                              >
+                                <item.icon className="mr-2 text-2xl" />
+                                <span className="ml-2">{item.title}</span>
+                              </a>
+                            </SidebarMenuButton>
+                          </TooltipTrigger>
+                          <TooltipContent
+                            side="right"
+                            align="center"
+                            hidden={state !== "collapsed"}
+                            className={`${
+                              theme === "dark" ? "bg-white text-black" : "bg-black text-white"
+                            }`}
+                          >
+                            {item.title}
+                          </TooltipContent>
+                        </Tooltip>
+                      </SidebarMenuItem>
+                    ))}
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </CollapsibleContent>
+            </SidebarGroup>
+          </Collapsible>
+        )}
+      </SidebarContent>
+      <SidebarFooter
+        className={`p-2 transition-colors duration-300 ${
+          theme === "dark" ? "bg-gray-800 hover:bg-gray-700" : "bg-gray-200 hover:bg-gray-300"
+        }`}
+      >
+        <SidebarMenu>
+          {footerItems.map((item) => (
+            <SidebarMenuItem
+              key={item.title}
+              className={`flex items-center p-2 transition-colors duration-200 ${
+                activeItem === item.title
+                  ? theme === "dark"
+                    ? "bg-gray-700"
+                    : "bg-gray-300"
+                  : theme === "dark"
+                  ? "hover:bg-gray-800"
+                  : "hover:bg-gray-200"
+              }`}
+            >
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <SidebarMenuButton asChild className="flex items-center w-full text-inherit no-underline text-lg">
+                    <a href={item.url} onClick={() => handleItemClick(item.title)} className="flex items-center w-full">
+                      <item.icon className="mr-2 text-2xl" />
+                      <span className="ml-2">{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </TooltipTrigger>
+                <TooltipContent
+                  side="right"
+                  align="center"
+                  hidden={state !== "collapsed"}
+                  className={`${
+                    theme === "dark" ? "bg-white text-black" : "bg-black text-white"
+                  }`}
+                >
+                  {item.title}
+                </TooltipContent>
+              </Tooltip>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
   );
 }
