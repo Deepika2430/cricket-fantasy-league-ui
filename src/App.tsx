d7f99b73a574777308591130a1d274f83f5de04a
Deepika2430 <deepika_pabbisetti@tecnics.com>
2025-02-06 16:53:51 +0530

    implement authentication context and routing for team selection and signout

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,43 @@
 import { useState, useEffect } from 'react';
-import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { Route, Routes, Navigate, BrowserRouter as Router } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Home from './components/Home';
 import Layout from './components/Layout';
 import Profile from './components/Profile';
 import Matches from './components/Matches';
+import Team from './components/TeamSelection';
+import Signout from './components/Signout';
+import { getUserFromToken } from './services/AuthService';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const navigate = useNavigate();
+  const [userId, setUserId] = useState('');
 
-  // useEffect(() => {
-  //   if (isAuthenticated) {
-  //     navigate('/dashboard');
-  //   } else {
-  //     navigate('/login');
-  //   }
-  // }, [isAuthenticated, navigate]);
+  useEffect(() => {
+    if (isAuthenticated)
+      setUserId(getUserFromToken(Cookies.get('authToken')));
+    if (userId)
+      setIsAuthenticated(true);
+  });
 
   return (
-    <Routes>
-      {/* <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-      <Route path="/home" element={isAuthenticated ? <Layout><Home /></Layout> : <Navigate to="/login" />} />
-      <Route path="/dashboard" element={isAuthenticated ? <Layout><Dashboard /></Layout> : <Navigate to="/login" />} />
-      <Route path="/profile" element={isAuthenticated ? <Layout><Profile /></Layout> : <Navigate to="/login" />} />
-      <Route path="*" element={<Navigate to={isAuthenticated ? "/dashboard" : "/login"} />} /> */}
-      <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-      <Route element={<Layout />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/matches" element={<Matches />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      <Route path="*" element={<Navigate to={isAuthenticated ? "/dashboard" : "/login"} />} />
-    </Routes>
+    <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated}/>} />
+          <Route element={<Layout />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/matches" element={<Matches />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+            <Route path="/signout" element={<Signout />} />
+          <Route path="*" element={<Navigate to={isAuthenticated ? "/dashboard" : "/login"} />} />
+        </Routes>
+    </AuthProvider>
   );
 }
 
